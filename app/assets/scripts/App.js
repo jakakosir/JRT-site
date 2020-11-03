182e60ca509cde5cb1f7c259b62a283a74d5855b
Jaka <jackkosir@gmail.com>
2020-11-03 13:02:51 +0100

    adding css styles and other stufff

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,6 @@
+import "../sass/main.scss";
+
+
+if (module.hot) {
+    module.hot.accept()
+}
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  entry: "./app/assets/scripts/App.js",
+  output: {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app"),
+  },
+  devServer: {
+    before: function (app, server) {
+      server._watch("./app/**/*.html");
+    },
+    contentBase: path.join(__dirname, "app"),
+    hot: true,
+    port: 3000,
+    host: "0.0.0.0",
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  plugins: [new TerserPlugin()],
+};
