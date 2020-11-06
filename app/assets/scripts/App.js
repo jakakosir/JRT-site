83d84953cb20f29b2181ad1aec1058b8923a380a
Jaka <jackkosir@gmail.com>
2020-11-06 10:45:13 +0100

    Adding menu toggling logic

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,8 @@
 import "../sass/main.scss";
+import OpenNavigation from "./modules/OpenNavigation";
 
+let openNavigation = new OpenNavigation();
 
 if (module.hot) {
-    module.hot.accept()
+  module.hot.accept();
 }
---
+++ app/assets/scripts/modules/OpenNavigation.js
@@ -0,0 +1,25 @@
+class OpenNavigation {
+  constructor() {
+    this.navBackground = document.getElementById("background");
+    this.navMenu = document.getElementById("menu");
+    this.menBar2 = document.getElementById("bar2");
+    this.menBar1 = document.getElementById("bar1");
+    this.menBar3 = document.getElementById("bar3");
+    this.navButton = document.querySelector(".navigation__button");
+    this.events();
+  }
+
+  events() {
+    this.navButton.addEventListener("click", () => this.toggleMenu());
+  }
+
+  toggleMenu() {
+    this.navBackground.classList.toggle("navigation__background_triggered");
+    this.navMenu.classList.toggle("menu__list_triggered");
+    this.menBar2.classList.toggle("navigation__button_icon-triggered");
+    this.menBar1.classList.toggle("navigation__button_icon--1-triggered");
+    this.menBar3.classList.toggle("navigation__button_icon--3-triggered");
+  }
+}
+
+export default OpenNavigation;
