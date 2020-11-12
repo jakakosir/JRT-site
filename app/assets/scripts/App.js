ec61c4ba61beb5d7f8a349d92b8e401e1f55d1d0
Jaka <jackkosir@gmail.com>
2020-11-12 18:06:52 +0100

    Including Reveal on Scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,10 @@
 import "../sass/main.scss";
 import OpenNavigation from "./modules/OpenNavigation";
+import RevealOnScroll from "./modules/RevealOnScroll";
 
+let revealOnScroll = new RevealOnScroll();
 let openNavigation = new OpenNavigation();
+
 
 if (module.hot) {
   module.hot.accept();
---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,57 @@
+import throttle from "lodash/throttle";
+import debounce from "lodash/debounce";
+
+class ReveallOnScroll {
+  constructor() {
+    this.parToRev = document.querySelectorAll(".reveal");
+    this.browserHight = window.innerHeight;
+    this.hideInitially();
+    this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
+    this.events();
+  }
+
+  events() {
+    window.addEventListener("scroll", this.scrollThrottle);
+    window.addEventListener(
+      "resize",
+      debounce(() => {
+        console.log("resize just ran");
+        this.browserHight = window.innerHeight;
+      }, 333)
+    );
+  }
+
+  calcCaller() {
+    console.log("scroll function run");
+    this.parToRev.forEach((e) => {
+      if (e.isRevealed == false) {
+        this.calculateIfScrolledTo(e);
+      }
+    });
+  }
+
+  calculateIfScrolledTo(e) {
+    if (window.scrollY + this.browserHight > e.offsetTop) {
+      console.log("element was calculated");
+      let scrollPercent =
+        (e.getBoundingClientRect().top / this.browserHight) * 100;
+      if (scrollPercent < 85) {
+        e.classList.add("reveal-par--is-visible");
+        e.isRevealed = true;
+        if (e.isLastItem) {
+          window.removeEventListener("scroll", this.scrollThrottle);
+        }
+      }
+    }
+  }
+
+  hideInitially() {
+    this.parToRev.forEach((e) => {
+      e.classList.add("reveal-par");
+      e.isRevealed = false;
+    });
+    this.parToRev[this.parToRev.length - 1].isLastItem = true;
+  }
+}
+
+export default ReveallOnScroll;
